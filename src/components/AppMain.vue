311c73d554ac45c93b325e09a83f09ee274b7df7
Danielateresa <dany.t_87@hotmail.it>
2022-11-16 17:36:21 +0100

    Added film posters

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -35,6 +35,9 @@ export default {
     <div class="movie_box">
       <ul>
         <li :movie="movie" v-for="movie in store.movies">
+          <div class="cover_box">
+            <img :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`" alt="">
+          </div>
           <p>Titolo: {{ movie.title }} {{ movie.name }}</p>
           <p>Titolo Originale: {{ movie.original_title }} {{ movie.original_name }}</p>
           <div class="flag">
@@ -46,11 +49,14 @@ export default {
               v-else="movie.original_language !== 'ja' && movie.original_language !== 'it' && movie.original_language !== 'en'"
               src="" alt="">
           </div>
-
           <p>Voto: {{ movie.vote_average }}</p>
+
+          <!-- /single movie-->
+
         </li>
       </ul>
     </div>
+    <!-- /.movie_box -->
 
 
   </div>
