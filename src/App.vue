49b25fcf3023c2da9ac7df0bf914eb907eaa6f30
Danielateresa <dany.t_87@hotmail.it>
2022-11-16 12:33:26 +0100

    Created api call

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
 <script >
+import { store } from './store.js';
+export default {
+  name: 'App',
+  components: {
+    //componenti
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  mounted() {
+    this.store.CallApi()
+    console.log(this.store);
+  }
+}
 
 
 </script>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,29 @@
 import { reactive } from 'vue';
+import axios from 'axios';
 
 export const store = reactive({
     api_Key: '2b45490e9387fca1e16c3bcb3a68dded',
-    query: ''
+    query: '',
+    error: null,
+    Movies: null,
+    categorySearch: null,
     //chiamata ajax
+    CallApi(url) {
+        const config = {
+            method: 'get',
+            url: 'get',
+            url: `https://api.themoviedb.org/3/search/movie?api_key=${this.api_Key}&language=en-US&query=${this.query}`,
+            params: {
+
+            }
+
+        };
+        axios(config)
+            .then(function (response) {
+                console.log((response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 })
