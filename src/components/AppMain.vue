b2dabe14d4b961de3a34aa087706c0dbf0d96f65
Danielateresa <dany.t_87@hotmail.it>
2022-11-16 14:32:12 +0100

    Working on milestone 1

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,8 +18,17 @@ export default {
 
   <div class="container">
 
+    <!-- trasformare in componente SearchComponent-->
     <input type="search" placeholder="Search movie" v-model="store.query">
     <button type="submit" class="btn btn-light ms-2" @click="$emit('searchData')">Search</button>
+
+    <!-- trasformare in componente MovieList/MovieItem-->
+    <div class="movie_box">v-if=""
+      <ul>
+        <li :movie="movie" v-for="movie in store.movies">{{ movie.title }}</li>
+      </ul>
+    </div>
+
 
   </div>
 
--- src/store.js
+++ src/store.js
@@ -2,30 +2,37 @@ import { reactive } from 'vue';
 import axios from 'axios';
 
 export const store = reactive({
-
+    api_key: '2b45490e9387fca1e16c3bcb3a68dded',
+    query: null,
     error: null,
-    Movies: null,
+    movies: null,
+    page: null,
     categorySearch: null,
     //chiamata ajax
     CallApi() {
-        console.log('search now');
+        //console.log('search now');
 
         /*  console.log(this.store.query); */
 
         const config = {
             method: 'get',
             url: 'get',
-            /* url: `https://api.themoviedb.org/3/search/movie?api_key=${this.api_Key}&language=en-US&query=${this.query}`, */
-            url: `https://api.themoviedb.org/3/search/movie`,
-            params: {
+            url: `https://api.themoviedb.org/3/search/movie?api_key=${store.api_key}&language=en-US&query=${store.query}`,
+            /*     url: `https://api.themoviedb.org/3/search/movie`, */
+            /* params: {
                 api_key: '2b45490e9387fca1e16c3bcb3a68dded',
-                query: 'matrix'
-            }
+                query: null,
+
+            } */
 
         };
         axios(config)
             .then(function (response) {
                 console.log((response.data));
+
+                store.movies = response.data.results;
+                /*     this.page = response.data.page; */
+                console.log(store.movies);
             })
             .catch(function (error) {
                 console.log(error);
