94b2c71c05f5404a6fa59ff45463a284a140b6c7
Danielateresa <dany.t_87@hotmail.it>
2022-11-16 16:09:59 +0100

    Added flags

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,6 +10,15 @@ export default {
     return {
       store
     }
+  },
+  methods: {
+    getFlag() {
+      if (store.movie.original_language === 'en') {
+        const eglishFlag = 'united-kingdom-flag-xs.png';
+      } else if (store.movie.original_language === 'it') {
+
+      }
+    }
   }
 }
 </script>
@@ -29,6 +38,12 @@ export default {
           <p>Titolo: {{ movie.title }}</p>
           <p>Titolo Originale: {{ movie.original_title }}</p>
           <p>Lingua: {{ movie.original_language }}</p>
+          <div class="flag">
+            <img v-if="movie.original_language === 'en'" src="../assets/img/united-kingdom-flag-xs.png" alt="">
+            <img v-else-if="movie.original_language === 'it'" src="../assets/img/italy-flag-xs.png" alt="">
+            <img v-else="movie.original_language === 'ja'" src="../assets/img/japan-flag-xs.png" alt="">
+          </div>
+
           <p>Voto: {{ movie.vote_average }}</p>
         </li>
       </ul>
