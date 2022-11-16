308affd11c38728f20a72194d017c07396887aec
Danielateresa <dany.t_87@hotmail.it>
2022-11-16 12:54:36 +0100

    Working on milestone 1

---
+++ src/components/AppMain.vue
@@ -0,0 +1,30 @@
+<script >
+import { store } from '../store.js';
+export default {
+  name: 'AppMain',
+  emits: ['searchData'],
+  components: {
+
+  },
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<template>
+
+  <div class="container">
+
+    <input type="search" placeholder="Search movie" v-model="store.query">
+    <button type="submit" class="btn btn light" @click="$emit('searchData')">Search</button>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
