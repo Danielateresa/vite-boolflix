f71e2deed5724de328601ce3e1e7e36f3cb4f70c
Danielateresa <dany.t_87@hotmail.it>
2022-11-18 15:17:54 +0100

    flag issue

--- src/components/SingleMovie.vue
+++ src/components/SingleMovie.vue
@@ -1,13 +1,33 @@
 <script>
-
 export default {
     name: 'SingleMovie',
     props: {
         movie: Object
     },
-
+    data() {
+        return {
+            lang: {
+                en: '../assets/img/united-kingdom-flag-xs.png',
+                it: '../assets/img/italy-flag-xs.png',
+                ja: '../assets/img/japan-flag-xs.png',
+            },
+            imageUrl: 'https://image.tmdb.org/t/p/',
+        }
+    },
+    methods: {
+        getFlag(lang) {
+            if (lang === 'en') {
+                return this.lang.en
+            } else if (lang === 'it') {
+                return this.lang.it
+            } else if (lang === 'ja') {
+                return this.lang.ja
+            } else {
+                return 'ciao'
+            }
+        }
+    }
 }
-
 
 
 </script>
@@ -17,7 +37,8 @@ export default {
     <div class="col-2 g-1 mb-4">
         <div class="card border-0">
 
-            <img :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`">
+            <img :src="`${imageUrl}w342${movie.poster_path}`">
+
 
 
             <div class="movie_info px-2 pt-4 h-100 overflow-auto">
@@ -25,10 +46,7 @@ export default {
                 <p><span>Titolo Originale: </span>{{ movie.original_title }} {{ movie.original_name }}</p>
                 <div class="flag">
                     <span>Lingua: {{ movie.original_language }}</span>
-                    <img v-if="movie.original_language === 'en'" src="../assets/img/united-kingdom-flag-xs.png" alt="">
-                    <img v-else-if="movie.original_language === 'it'" src="../assets/img/italy-flag-xs.png" alt="">
-                    <img v-else-if="movie.original_language === 'ja'" src="../assets/img/japan-flag-xs.png" alt="">
-                    <span v-else alt="">Non disponibile</span>
+                    <img :src="getFlag(movie.original_language)" alt="">
                 </div>
                 <p><span>Voto: </span>
                     <font-awesome-icon icon="fa-solid fa-star" v-for="(i) in 5"
@@ -36,6 +54,7 @@ export default {
 
                 </p>
                 <p>{{ movie.overview }}</p>
+
             </div>
         </div>
     </div>
