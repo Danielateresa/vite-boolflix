31d3bddc86785ccdd1c78fd507176056a139a7e1
Danielateresa <dany.t_87@hotmail.it>
2022-11-18 12:51:39 +0100

    Modified api call

--- src/store.js
+++ src/store.js
@@ -6,31 +6,38 @@ export const store = reactive({
     query: null,
     error: null,
     movies: null,
-    showInfo: false,
-    //chiamata ajax
+    movieUrl: 'https://api.themoviedb.org/3/search/movie',
+    tvUrl: 'https://api.themoviedb.org/3/search/tv',
+
+    GenerateUrlMovies() {
+        const url = `${store.movieUrl}?api_key=${store.api_key}&query=${store.query}`;
+        return axios.get(url);
+    },
+    GenerateUrlTv() {
+        const url = `${store.tvUrl}?api_key=${store.api_key}&query=${store.query}`;
+        return axios.get(url);
+    },
+
     CallApi() {
         //console.log('search now');
 
         //console.log(this.store.query); //
 
-        const config = {
-            method: 'get',
-            url: `https://api.themoviedb.org/3/search//multi?api_key=${store.api_key}&language=en-US&query=${store.query}&page=1&include_adult=falsehttps://api.themoviedb.org/3/search/tv?api_key=${store.api_key}&page=1&query=${store.query}&include_adult=false`,
-            /*     url: `https://api.themoviedb.org/3/search/movie`, */
-            /* params: {
-                api_key: '2b45490e9387fca1e16c3bcb3a68dded',
-                query: null,
 
-            } */
+        /*  axios(store.config)
+             .then(function (response) {
+                
+                 store.movies = response.data.results;
+                 /*     this.page = response.data.page; 
+                 console.log(store.movies);
+                 store.query = '';
+             }) */
 
-        };
-        axios(config)
-            .then(function (response) {
-                console.log((response.data));
-                store.movies = response.data.results;
-                /*     this.page = response.data.page; */
-                console.log(store.movies);
-                store.query = '';
+
+        Promise.all([store.GenerateUrlMovies(), store.GenerateUrlTv()])
+            .then(results => {
+                console.log(results[1].data.results);
+                store.movies = results[1].data.results;
             })
             .catch(function (error) {
                 console.log(error);
